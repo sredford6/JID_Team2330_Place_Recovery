0a63de238d26299b3289de1a1ca426c95cb35111
Ishan Arya <ishan.arya130@gmail.com>
2022-01-26 13:42:44 -0500

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const auth = require("./auth.json");
 
-const dbURI = "mongodb://localhost/authentication";
+const dbURI = auth.mongodb.uri;
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: auth.mongodb.db,
+  user: auth.mongodb.user,
+  pass: auth.mongodb.pass,
+};
 app.use(express.json());
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbURI, dbOptions);
 const db = mongoose.connection;
 
 db.on("error", (err) => {
@@ -13,6 +21,7 @@ db.on("error", (err) => {
 });
 db.once("open", () => {
   console.log("DB started successfully");
+  console.log(`DB name: ${db.name}`);
 });
 
 app.listen(2400, () => {
