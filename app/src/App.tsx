ac7ad6d6a23c0762b3ecfdf8033a47fe3221fe38
Aizhan <aizhan.nurmanova2000@gmail.com>
2022-01-27 18:27:03 -0500

    updated registration page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,17 +5,29 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import RegistrationScreen from './screens/RegistrationScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import LocationScreen from './screens/LocationScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <RegistrationScreen />
+        <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Registation" component={RegistrationScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          
+        </Stack.Navigator>
+    </NavigationContainer>
         <StatusBar />
       </SafeAreaProvider>
     );
