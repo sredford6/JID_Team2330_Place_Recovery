ce1ae87b12daebb113795c8d34b760da50503339
Ishan Arya <ishan.arya130@gmail.com>
2022-01-30 18:08:29 -0500

    added route to get reset code

---
+++ server_ts/src/helpers/nodemailer.ts
@@ -0,0 +1,62 @@
+import nodemailer from "nodemailer";
+import * as SMTPTransport from "nodemailer/lib/smtp-transport";
+import * as Mail from "nodemailer/lib/mailer";
+import { google } from "googleapis";
+const OAuth2 = google.auth.OAuth2;
+
+import authObj from "config/auth.json";
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    authObj.google.client_id,
+    authObj.google.client_secret,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: authObj.google.refresh_token,
+  });
+
+  const accessToken: string = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transportObj: SMTPTransport.Options = {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: authObj.google.email,
+      clientId: authObj.google.client_id,
+      clientSecret: authObj.google.client_secret,
+      refreshToken: authObj.google.refresh_token,
+      accessToken,
+    },
+  };
+  const transporter = nodemailer.createTransport(transportObj);
+
+  return transporter;
+};
+
+export async function sendEmail(emailOptions: Mail.Options) {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(emailOptions);
+}
+
+/*
+Example:
+
+sendEmail({
+  subject: "Test",
+  text: "I am sending an email from nodemailer!",
+  to: "aryaishn@gmail.com",
+  from: authObj.google.email,
+});
+
+*/
--- server_ts/src/models/user.model.ts
+++ server_ts/src/models/user.model.ts
@@ -6,6 +6,7 @@ export interface IUser {
   phoneNumber: string;
   firstName: string;
   lastName: string;
+  resetCode?: string;
 }
 
 const UserSchema = new Schema<IUser>({
@@ -14,6 +15,7 @@ const UserSchema = new Schema<IUser>({
   phoneNumber: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  resetCode: { type: String, required: false },
 });
 
 export default model<IUser>("User", UserSchema);
