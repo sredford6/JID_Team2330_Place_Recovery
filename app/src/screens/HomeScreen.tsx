b79e9f4c419feca9a33b7334b65fd80c73c8323e
aschweitzer7 <aschweitzer7@gatech.edu>
2022-02-14 22:36:54 -0500

    questionnaire button to home screen

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -1,18 +1,46 @@
-import { StyleSheet, Button } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  TouchableHighlight,
+  TextInput,
+  KeyboardAvoidingView,
+  ImageBackground,
+} from "react-native";
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LocationScreen from './LocationScreen';
+import axios from "axios";
+import { ScrollView } from "react-native";
 
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   
   return (
-    <View style={styles.container}>
-      <Button title="Start Questionnaire" />
-    </View>
+<ScrollView
+      contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+    >
+    
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: 40,
+              }}
+            >
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('QuestionnaireScreen')}
+                activeOpacity={0.85}
+              >
+                <Text style={styles.buttonTextWhite}>Start Questionnaire</Text>
+              </TouchableOpacity>
+            </View>
+            
+    </ScrollView>
   );
 }
 
@@ -26,6 +54,22 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  button: {
+    width: 250,
+    height: 45,
+    backgroundColor: "#072B4F",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginTop: 5,
+  },
+  buttonTextWhite: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
