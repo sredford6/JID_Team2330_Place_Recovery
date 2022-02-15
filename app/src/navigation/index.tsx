357eb66853e32c6585627e90ff810af7f20dffa3
aschweitzer7 <aschweitzer7@gatech.edu>
2022-02-14 22:37:08 -0500

    navigation

--- app/src/navigation/index.tsx
+++ app/src/navigation/index.tsx
@@ -17,6 +17,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LocationScreen from '../screens/LocationScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import QuestionnaireScreen from '../screens/QuestionnaireScreen';
 import OpeningScreen from '../screens/OpeningScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
