f0ced69dec40f7f461262decde02cc2b1b449536
Ishan Arya <ishan.arya130@gmail.com>
2022-04-12 20:47:40 -0400

    started working on data endpoint

--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
+import * as csv from "@fast-csv/format";
 const router = express.Router();
 
-router.get('/users', async (req, res: Response) => {
-  
+import User, { IUser } from "models/user.model";
+
+router.get("/users", async (req, res: Response) => {
+  try {
+    const users: IUser = (await User.find({})) as unknown as IUser;
+    console.log(users);
+    res.status(200).json(users);
+  } catch (error: any) {
+    console.error(error);
+    res.status(error.httpCode || 500).json(error);
+  }
 });
 
 export default router;
