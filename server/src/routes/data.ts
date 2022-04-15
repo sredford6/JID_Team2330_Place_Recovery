3ec0c1a684302cb33c9d2403af7fce4c75fa1871
Ishan Arya <ishan.arya130@gmail.com>
2022-04-14 21:33:33 -0400

    updated data users endpoint

--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import * as csv from "@fast-csv/format";
+import { format, writeToString } from "@fast-csv/format";
 const router = express.Router();
 
 import User, { IUser } from "models/user.model";
@@ -7,8 +7,20 @@ import { verify } from "middleware";
 
 router.get("/users", async (req, res: Response) => {
   try {
-    const users: IUser = (await User.find({})) as unknown as IUser;
-    res.status(200).json(users);
+    const users: IUser[] = await User.find({}).select(
+      "-answers -password -resetCode -resetTries"
+    );
+    const csvStream = format({ headers: true });
+    // const csvString = await writeToString(users);
+    // // console.log(csvString);
+    res.type("text/csv");
+    csvStream.pipe(res);
+    for (let user of users) {
+      console.log(user);
+      csvStream.write(user);
+    }
+    csvStream.end();
+    // res.status(200).json(users);
   } catch (error: any) {
     console.error(error);
     res.status(error.httpCode || 500).json(error);
