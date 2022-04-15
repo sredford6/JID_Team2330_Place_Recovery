1d584a50d4b7f9304019fab30e959601ea30bc9f
Ishan Arya <ishan.arya130@gmail.com>
2022-04-14 23:01:47 -0400

    answers csv working

--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { format } from "@fast-csv/format";
+import { Document, Types } from "mongoose";
 const router = express.Router();
 
 import User, { IUser } from "models/user.model";
+import { IAnswer, IAnswers } from "models/answer.model";
 import { verify } from "middleware";
+import { formatIAnswers } from "helpers/formatters";
 
 router.get("/users", async (req, res: Response) => {
   try {
@@ -16,6 +19,26 @@ router.get("/users", async (req, res: Response) => {
     csvStream.pipe(res);
     for (let user of users) {
       csvStream.write(user.toObject());
+    }
+    csvStream.end();
+  } catch (error: any) {
+    console.error(error);
+    res.status(error.httpCode || 500).json(error);
+  }
+});
+
+router.get("/answers/:user", async (req, res: Response) => {
+  try {
+    const userId = req.params.user;
+    const user = await User.findOne({ _id: userId });
+    const { answers } = user;
+    answers.sort((a, b) => b.answers.length - a.answers.length);
+    const csvStream = format({ headers: true });
+    res.attachment(`${user.lastName}_${user.firstName}_Answers.csv`);
+    res.type("text/csv");
+    csvStream.pipe(res);
+    for (let answer of answers) {
+      csvStream.write(formatIAnswers(answer));
     }
     csvStream.end();
   } catch (error: any) {
