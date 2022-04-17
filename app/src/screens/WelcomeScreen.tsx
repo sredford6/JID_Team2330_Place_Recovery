af023515ede84980a06672a5e70d377080e29aff
Jianqi Lin <DorianLin@outlook.com>
2022-04-17 18:37:55 -0400

    welcome screen

--- app/src/screens/WelcomeScreen.tsx
+++ app/src/screens/WelcomeScreen.tsx
@@ -1,24 +1,71 @@
+import React, { useState, useRef, useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Animated } from "react-native";
 
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Onboarding from '../components/Onboarding';
+import OnboardingItem from "../components/OnboardingItem";
+import slides from "../slides";
+import NextButton from "../components/NextButton";
+import Paginator from "../components/Paginator";
+import { WelcomeContext } from "../navigation/context";
 
-export default function WelcomeScreen  ({navigation} ) {
-    return (
-        <View style={styles.container}>
-            <Onboarding />
-            <StatusBar style="auto" />
-        </View>
-    );
-};
+export default function WelcomeScreen() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = useRef(null);
+  const { setIsFirstTime, isFirstTime } = useContext(WelcomeContext);
+
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+  const scrollTo = () => {
+    if (currentIndex < slides.length - 1) {
+      slidesRef.current.scrollToIndex({ index: currentIndex + 1 });
+    } else {
+      console.log("Last item.");
+      setIsFirstTime(false);
+      console.log("isFirstTime", isFirstTime);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+
+      <Paginator data={slides} scrollX={scrollX} />
+      <NextButton
+        scrollTo={scrollTo}
+        percentage={(currentIndex + 1) * (100 / slides.length)}
+      />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
