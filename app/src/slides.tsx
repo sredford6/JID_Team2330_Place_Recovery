c12f86a48d22a92c1e8db69984bfff8367e210e4
byron135 <70074940+byron135@users.noreply.github.com>
2022-04-17 01:48:19 -0400

    WelcomeScreen Info
    Need to update the description and title later

---
+++ app/src/slides.tsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: '1',
+        title: 'Check your mood',
+        description: 'track your location',
+        image: require('./assets/images/walking.png'),
+    },
+    {
+        id: '2',
+        title: 'track your location ',
+        description: 'track your location',
+        image: require('./assets/images/enjoy.png'),
+    },
+    {
+        id: '3',
+        title: 'Instant Notifications',
+        description: 'call you 3 times a day',
+        image: require('./assets/images/man.png'),
+    },
+    {
+        id: '4',
+        title: 'track your location',
+        description: 'track your location',
+        image: require('./assets/images/location.png'),
+    },
+];
