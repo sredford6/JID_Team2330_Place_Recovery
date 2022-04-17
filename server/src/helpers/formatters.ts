3657ac4d0bf1855c05f4ee7dfeb951a968c44f55
Ishan Arya <ishan.arya130@gmail.com>
2022-04-17 17:58:57 -0400

    added all user get data

--- server/src/helpers/formatters.ts
+++ server/src/helpers/formatters.ts
@@ -1,9 +1,18 @@
 import { IAnswers } from "models/answer.model";
+import { IUser } from "models/user.model";
 
-export function formatIAnswers(answer: IAnswers) {
+function formatIAnswers(
+  answer: IAnswers,
+  userEmail: string,
+  day: number,
+  index: number
+) {
   const answers = answer.answers;
   const ansObj: any = {};
   ansObj.datetime = answer.datetime;
+  ansObj.day = day;
+  ansObj.index = index;
+  ansObj.email = userEmail;
   ansObj.questionnaire = answer.questionnaire;
   ansObj.location = answer.location;
   for (let i = 0; i < answers.length; i++) {
@@ -12,4 +21,51 @@ export function formatIAnswers(answer: IAnswers) {
   }
 
   return ansObj;
+}
+
+export function formatIAnswersSingleUser(user: IUser) {
+  const answers: IAnswers[] = user.answers;
+  const formattedAnswers: any[] = [];
+
+  answers.sort((a, b) => a.datetime.getTime() - b.datetime.getTime());
+
+  for (let answer of answers) {
+    if (formattedAnswers.length === 0) {
+      formattedAnswers.push(formatIAnswers(answer, user.email, 0, 0));
+      continue;
+    }
+    const lastAnswer = formattedAnswers.at(-1);
+    if (checkSameDate(lastAnswer.datetime, answer.datetime)) {
+      formattedAnswers.push(
+        formatIAnswers(answer, user.email, lastAnswer.day, lastAnswer.index + 1)
+      );
+      continue;
+    }
+    formattedAnswers.push(
+      formatIAnswers(answer, user.email, lastAnswer.day + 1, 0)
+    );
+  }
+
+  return formattedAnswers.sort(
+    (a, b) => Object.keys(b).length - Object.keys(a).length
+  );
+}
+
+export function formatIAnswersMultipleUsers(users: IUser[]) {
+  const formattedAnswers: any[] = [];
+  for (const user of users) {
+    formattedAnswers.push(...formatIAnswersSingleUser(user));
+  }
+
+  return formattedAnswers.sort(
+    (a, b) => Object.keys(b).length - Object.keys(a).length
+  );
+}
+
+function checkSameDate(a: Date, b: Date) {
+  return (
+    a.getFullYear() === b.getFullYear() &&
+    a.getMonth() === b.getMonth() &&
+    a.getDate() === b.getDate()
+  );
 }
