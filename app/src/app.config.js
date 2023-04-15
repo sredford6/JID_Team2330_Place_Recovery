d9015d831a518f8a668f990b774cd4f506a3a6b6
sredford6 <95582784+sredford6@users.noreply.github.com>
2023-04-15 16:15:21 -0400

    Create app.config.js

---
+++ app/src/app.config.js
@@ -0,0 +1,9 @@
+module.exports = ({ config }) => ({
+  name: config.name,
+  slug: "jid_team2330_place_recovery",
+  extra: {
+    eas: {
+      projectId: "1b75ddb5-31d3-4150-b409-566d6b0b07b0"
+    }
+  },
+});
